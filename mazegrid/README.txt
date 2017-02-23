f9a2470909f63cd9fe3e4cf400862c24386287ba
Jaehyung Park <neojinkazu@gmail.com>
2017-02-23 10:44:24 -0800

    minor changes

--- mazegrid/README.txt
+++ mazegrid/README.txt
@@ -1,5 +1,3 @@
 Functions to convert a Black-White maze image to a 2D array
 
-run with $python maze.py >> output.txt
-
-
+run with $python maze.py
--- mazegrid/maze.py
+++ mazegrid/maze.py
@@ -5,18 +5,34 @@ import glob
 from os import path
 from PIL import Image
 
+WALL = 1
+PATH = 0
+
 f = open('output.txt','w')
 
-# somehow, the image generated from otsu's method is not grayscale so I had to convert it to grayscale
-# and save the file.
+# cropped out the maze image that starts with the black border line
 img = Image.open('bw_maze_mod.png','r')
 
 pixel_value = list(img.getdata()) # pixel_value is a list that contains all the pixel values
+# (0,0,0,255) as black and (255,255,255,255) as white
 
-print img.format, img.size, img.mode # check size and type of image
+# set newline each width and convert black(wall) to 1 and white(path) to 0
+# save the outputs to output.txt
+
+for i in range(len(pixel_value)):
+	if pixel_value[i] == (0,0,0,255):
+		pixel_value[i] = WALL
+	if pixel_value[i] == (255,255,255,255):
+		pixel_value[i] = PATH
 
 width, height = Image.open(open('bw_maze_mod.png')).size
-print width*height
+mazesize = width*height
+print "width of maze: {}".format(width)
+print "height of maze: {}".format(height)
+print "total number of pixels: {}".format(mazesize)
 
-print(pixel_value)
+for i in range(1/width):
+	join(pixel_value[i*width:(i+1)*width]) + "\n"
+
+print >> f, pixel_value
 f.close()
